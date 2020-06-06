62a3e2b9fb3a52e9ad35fe69f1a573dca7d6f06e
Abdelrahman Khalid <abdelrahman.elzohry2014@gmail.com>
2020-06-06 16:37:12 +0200

    finished the user authentication

--- Threeks/routes.py
+++ Threeks/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, flash, url_for, redirect, request
+from flask_login import current_user, login_user, logout_user, login_required
 from Threeks.forms import SignupForm, LoginForm
 from Threeks.models import User, Post
 from Threeks import app, db, bcrypt
@@ -22,7 +23,7 @@ posts=[
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', posts=posts, username='abdelrahman')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
@@ -30,6 +31,8 @@ def about():
 
 @app.route('/signup', methods=['GET','POST'])
 def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = SignupForm()
     if request.method == 'POST':
         if form.validate_on_submit():
@@ -45,13 +48,30 @@ def signup():
 
 @app.route('/login', methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if request.method == 'POST':
         if form.validate_on_submit():
             user = User.query.filter_by(email=form.email.data).first()
+            next_page = request.args.get('next')
             if user and bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember_me.data)
                 flash(f"You have Logged in Successfully", "success")
-                return redirect(url_for('home'))
+                if next_page:
+                    return redirect(next_page)
+                else:
+                    return redirect(url_for('home'))
             else:
                 flash("Please Check your email and password", "danger")
     return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', profile=True)
