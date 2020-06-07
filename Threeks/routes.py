493dea99f58ed0dd7eb715c574db4679fccc9bf5
Abdelrahman Khalid <abdelrahman.elzohry2014@gmail.com>
2020-06-07 11:20:33 +0200

    added the functionality to update the profile pic

--- Threeks/routes.py
+++ Threeks/routes.py
@@ -3,7 +3,10 @@ from flask_login import current_user, login_user, logout_user, login_required
 from Threeks.forms import SignupForm, LoginForm, UpdateProfileForm
 from Threeks.models import User, Post
 from Threeks import app, db, bcrypt
+from PIL import Image
 import datetime
+import secrets
+import os
 
 posts=[
     {
@@ -71,11 +74,46 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
+def save_image(image_data):
+    random_name = secrets.token_hex(8)
+    _, img_ext = os.path.splitext(image_data.filename)
+    image_name = random_name + img_ext
+    image_path = os.path.join(app.root_path, r'static\images' ,image_name)
+
+    img = Image.open(image_data)
+    img_width, img_height = img.size
+    # the crop function takes a tuble that is formatted like this in pixels
+    # (left, top, right, bottom) and every measure should start from the point
+    # (0,0) in the top left of the image
+    # if the image is a square or close to a one it will be saved without cropping
+    if img_width == img_height or abs(img_width - img_height) < 10:
+        img.save(image_path)
+    # if it's not it will crop it to its bigger center square
+    else:
+        if img_height > img_width:
+            img = img.crop((0, (img_height - img_width) / 2, img_width, img_height - ((img_height - img_width) / 2)))
+        else:
+            img = img.crop(((img_width - img_height) / 2, 0, img_width - ((img_width - img_height) / 2), img_height))
+
+    # then it will resize the image to decrease its size
+    # the thumbnail function differes from the resize function that it keeps the aspect ratio of the image
+    img.thumbnail((200, 200))
+    # then saving it
+    img.save(image_path)
+    
+    # returns the name of the image to be stored in the database
+    return image_name
+
 @app.route('/profile', methods=['POST','GET'])
 @login_required
 def profile():
     form = UpdateProfileForm()
+    print(app.root_path)
     if form.validate_on_submit():
+        if form.profile_image.data:
+            current_user.image_file = save_image(form.profile_image.data)
+            db.session.commit()
+        
         current_user.email = form.email.data
         current_user.username = form.username.data
         db.session.commit()
